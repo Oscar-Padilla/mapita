68caa700c0fa332531f8e59aaaf7bbf087d05556
Oscar <oscarbarcenas417@gmail.com>
2023-11-12 23:47:27 -0600

    bencha

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\UserLocationController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +23,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Rutas User
+Route::post('/user_store', [UserController::class, 'store']);
+Route::get('/user_index', [UserController::class, 'index']);
+
+//rutas Rol
+Route::post('/rol_store', [RolController::class, 'store']);
+Route::get('/rol_index', [RolController::class, 'index']);
+
+//Rutas Location
+Route::post('/loc_store', [LocationController::class, 'store']);
+Route::get('/loc_index', [LocationController::class, 'index']);
+
+//Rutas Review
+Route::post('rev_store', [ReviewController::class, 'store']);
+Route::get('rev_index', [ReviewController::class, 'index']);
+
+//Rutas user Location
+Route::post('usLoc_store', [UserLocationController::class, 'store']);
+Route::get('usLoc_index', [UserLocationController::class, 'index']);
+
+//Rutas Category
+Route::post('cat_store', [CategoryController::class, 'store']);
+Route::get('cat_index', [CategoryController::class, 'index']);
+
